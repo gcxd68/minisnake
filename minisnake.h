dc8c6a6f52f7c021742c067c1b69bc1784545f5f
Gregory Dosch <gdosch@k2r2p8.42mulhouse.fr>
2025-09-08 12:33:40 +0200

    gdosch | input refactor + small changes

--- minisnake.h
+++ minisnake.h
@@ -5,6 +5,7 @@
 # include <signal.h>
 # include <stdio.h>
 # include <stdlib.h>
+# include <string.h>
 # include <sys/ioctl.h>
 # include <termios.h>
 # include <time.h>
--- minisnake_core.c
+++ minisnake_core.c
@@ -2,33 +2,28 @@
 
 struct termios	g_savedTerm;
 
-static int	keyboardHit(void) {
-	int	ch, oldf;
-
-	oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
-	fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
-	ch = getchar();
-	fcntl(STDIN_FILENO, F_SETFL, oldf);
-	ungetc(ch, stdin);
-	return (ch != EOF) ? 1 : 0;
-}
-
 static void	handleInput(t_data *d) {
+	int oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
+	char keys[] = "aAwWdDsS", dirs[] = {LEFT,LEFT,UP,UP,RIGHT,RIGHT,DOWN,DOWN};
+	char *pos;
+	
 	d->dir[1] = d->dir[0];
-	if (!keyboardHit())
-		return;
-	char input = getchar();
-	if ((input == 'a' || input == 'A') && d->dir[0] != RIGHT) d->dir[0] = LEFT;
-	else if ((input == 'd' || input == 'D') && d->dir[0] != LEFT) d->dir[0] = RIGHT;
-	else if ((input == 'w' || input == 'W') && d->dir[0] != DOWN) d->dir[0] = UP;
-	else if ((input == 's' || input == 'S') && d->dir[0] != UP) d->dir[0] = DOWN;
-	else if (input == 'x' || input == 'X') d->gameOver = 1;
+	fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
+	int c = getchar();
+	fcntl(STDIN_FILENO, F_SETFL, oldf);
+	if (c == EOF) return;
+	if (c == 'x' || c == 'X')
+		d->gameOver = 1;
+	else if ((pos = strchr(keys, c))) {
+		if ((dirs[pos - keys] + 1) >> 1 != (d->dir[0] + 1) >> 1)
+			d->dir[0] = dirs[pos - keys];
+	}
 }
 
 static void	handleLogic(t_data *d) {
 	if (d->grow && d->grow--)
 		d->size++;
-	for (int i = d->size; i > 0; i--) {
+	for (int i = d->size; i; i--) {
 		d->x[i] = d->x[i - 1];
 		d->y[i] = d->y[i - 1];
 	}
@@ -54,9 +49,10 @@ static void	updateDisplay(t_data *d) {
 
 	printf(CURSOR_POS " ", d->y[d->size] + 2, d->x[d->size] + 2);
 	printf(CURSOR_POS "@", d->fruitY + 2, d->fruitX + 2);
+	if (d->size > 1)
+		printf(CURSOR_POS "%s", d->y[1] + 2, d->x[1] + 2,
+			(d->dir[0] + d->dir[1] == 5) ? corner[(d->dir[0] % 2)] : "▚");
 	printf(CURSOR_POS "%s", d->y[0] + 2, d->x[0] + 2, head[d->dir[0]]);
-	if (d->size > 1) printf(CURSOR_POS "%s", d->y[1] + 2, d->x[1] + 2,
-		(d->dir[0] + d->dir[1] == 5) ? corner[(d->dir[0] % 2)] : "▚");
 	printf(CURSOR_POS "%d\n\n\n", d->height + 3, 8, d->score);
 	fflush(stdout);
 }
